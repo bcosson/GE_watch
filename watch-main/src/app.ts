2c0ceaab3979fd62066652a9898314b209bda0db
bcoss <bastou999@gmail.com>
2024-02-15 11:21:55 +0100

    endpoints

---
+++ watch-main/src/app.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { Watch } from "./watch";
+
+const app = express();
+const port = 3000;
+
+const watch = new Watch();
+console.log("Time is", watch.get_time());
+
+app.use(express.static("public"));
+
+app.get("/time", (req, res) => {
+  res.send(watch.get_time());
+});
+
+app.get("/toggleMode", (req, res) => {
+  watch.on_mode_click();
+  res.send();
+});
+
+app.get("/increaseTime", (req, res) => {
+  watch.increment_time();
+  res.send();
+});
+
+app.get("/toggleLight", (req, res) => {
+  watch.on_light_click();
+  res.send();
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
